862e34373b0498623cdaa6dc5fb84b3d6cabf7ca
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2018-07-28 23:21:27 +0200

    Upgrade buildings and add farm building

--- src/components/Cookie.vue
+++ src/components/Cookie.vue
@@ -1,18 +1,36 @@
 <template>
-    <div>
+    <div id="mainContainer">
         <div id="cookiesContainer">
             <p id="cookiesCount">{{ cookiesCount.toFixed(0) }} cookies</p>
             <p id="cookiesPerSecond">{{ cookiesPerSecond.toFixed(2) }} cookies per second</p>
-            <img id="cookie" width="350" height="350" src="../assets/images/PerfectCookie.png" @click="increment($event)"/>
+            <img id="cookie" width="350" height="350" src="../assets/images/PerfectCookie.png" @click="increment('cookie')"/>
         </div>
 
-        <div id="buildings" v-for="(data, building) in buildings">
-            <a href="#" v-bind:disabled="cookiesCount < data.price">
-                <div v-bind:id="building" @click="increment($event)">
-                    <p class="buildingCount">{{ data.count + " " + building }}</p>
-                    <p class="buildingPrice">Price : {{ data.price.toFixed(0) }} cookies</p>
-                </div>
-            </a>
+        <div id="upgradesContainer">
+            <h3>UPGRADES</h3>
+            <div id="upgrades" v-for="upgrade in upgrades">
+                <a href="#" v-bind:disabled="cookiesCount < upgrade.price">
+                    <div @click="improvement(upgrade.id)">
+                        <p class="upgradeName">{{ upgrade.name }}</p>
+                        <p class="upgradeDescription">{{ upgrade.description }}</p>
+                        <p class="upgradePrice">Price : {{ upgrade.price }} cookies</p>
+                    </div>
+                </a>
+                <hr>
+            </div>
+        </div>
+
+        <div id="buildingsContainer">
+            <h3>BUILDINGS</h3>
+            <div id="buildings" v-for="(data, building) in buildings">
+                <a href="#" v-bind:disabled="cookiesCount < data.price">
+                    <div @click="increment(building)">
+                        <p class="buildingCount">{{ data.count + " " + building }}</p>
+                        <p class="buildingPrice">Price : {{ data.price.toFixed(0) }} cookies</p>
+                    </div>
+                </a>
+                <hr>
+            </div>
         </div>
     </div>
 </template>
@@ -23,16 +41,20 @@
     export default {
         name: "Cookie",
         methods: {
-            increment(event) {
-                var clickedElement = event.currentTarget.id;
-                this.$store.commit('increment', clickedElement);
+            increment(selectedBuilding) {
+                this.$store.commit('increment', selectedBuilding);
+            },
+
+            improvement(selectedUpgrade) {
+                this.$store.commit('improvement', selectedUpgrade);
             }
         },
         computed: {
             ...mapGetters([
                 'cookiesCount',
                 'cookiesPerSecond',
-                'buildings'
+                'buildings',
+                'upgrades'
             ])
         },
         mounted() {
@@ -55,5 +77,13 @@
     }
     a[disabled] {
         color: grey;
+    }
+    #mainContainer {
+        display: flex;
+    }
+    #cookiesContainer, #upgradesContainer, #buildingsContainer {
+        flex: 1;
+        padding-left: 5px;
+        padding-right: 5px;
     }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import lodash from 'lodash';
 
 Vue.use(Vuex);
 
@@ -18,35 +19,158 @@ export default new Vuex.Store({
                 count: 0,
                 price: 100,
                 cps: 1
+            },
+            farm: {
+                count: 0,
+                price: 1100,
+                cps: 8
             }
-        }
+        },
+        upgrades: {
+            cursor: [
+                {
+                    id: 1,
+                    name: "Reinforced index finger",
+                    description: "The mouse and cursors are twice as efficient. \"prod prod\"",
+                    need: 1,
+                    price: 100,
+                    active: false
+                },
+                {
+                    id: 2,
+                    name: "Carpal tunnel prevention cream",
+                    description: "The mouse and cursors are twice as efficient. \"it... it hurts to click...\"",
+                    need: 1,
+                    price: 500,
+                    active: false
+                },
+                {
+                    id: 3,
+                    name: "Ambidextrous",
+                    description: "The mouse and cursors are twice as efficient. \"Look ma, both hands!\"",
+                    need: 10,
+                    price: 10000,
+                    active: false
+                },
+            ],
+            grandma: [
+                {
+                    id: 4,
+                    name: "Forwards from grandma",
+                    description: "Grandmas are twice as efficient. \"RE:RE:thought you'd get a kick out of this ;))\"",
+                    need: 1,
+                    price: 1000,
+                    active: false
+                },
+                {
+                    id: 5,
+                    name: "Steel-plated rolling pins",
+                    description: "Grandmas are twice as efficient. \"Just what you kneaded.\"",
+                    need: 5,
+                    price: 5000,
+                    active: false
+                },
+                {
+                    id: 6,
+                    name: "Lubricated dentures",
+                    description: "Grandmas are twice as efficient. \"squish\"",
+                    need: 25,
+                    price: 50000,
+                    active: false
+                },
+            ],
+            farm: [
+                {
+                    id: 7,
+                    name: "Cheap hoes",
+                    description: "Farms are twice as efficient. \"Rake in the dough!\"",
+                    need: 1,
+                    price: 11000,
+                    active: false
+                },
+                {
+                    id: 8,
+                    name: "Fertilizer",
+                    description: "Farms are twice as efficient. \"It's chocolate, I swear.\"",
+                    need: 5,
+                    price: 55000,
+                    active: false
+                },
+                {
+                    id: 9,
+                    name: "Cookie trees",
+                    description: "Farms are twice as efficient.\"A relative of the breadfruit.\"",
+                    need: 25,
+                    price: 550000,
+                    active: false
+                },
+            ]
+        },
+        displayedUpgrades: []
     },
     getters: {
         cookiesCount: state => state.cookiesCount,
         cookiesPerSecond: state => state.cookiesPerSecond,
-        buildings: state => state.buildings
+        buildings: state => state.buildings,
+        upgrades: state => state.displayedUpgrades
     },
     mutations: {
-        increment(state, clickedElement) {
-            const {cookiesCount, cookiesPerSecond, cookiesOnClick, buildings} = state;
-            if (clickedElement === "cookie") {
+        increment(state, selectedBuilding) {
+            const {cookiesCount, cookiesPerSecond, cookiesOnClick, buildings, upgrades} = state;
+            if (selectedBuilding === "cookie") {
                 state.cookiesCount += cookiesOnClick;
-            } else if (clickedElement === "cookiesPerSecond") {
+            } else if (selectedBuilding === "cookiesPerSecond") {
                 /*
                 Incrémente le compteur de cookies tous les centièmes de seconde par le nombre de cookies par seconde divisé par 100
                  */
                 state.cookiesCount += (cookiesPerSecond / 100);
-            } else if (clickedElement === "cursor" && cookiesCount >= buildings.cursor.price) {
-                state.buildings.cursor.count++;
-                state.cookiesPerSecond += buildings.cursor.cps;
-                state.cookiesCount -= buildings.cursor.price;
-                state.buildings.cursor.price = buildings.cursor.price * 1.15;
-            } else if (clickedElement === "grandma" && cookiesCount >= buildings.grandma.price) {
-                state.buildings.grandma.count++;
-                state.cookiesPerSecond += buildings.grandma.cps;
-                state.cookiesCount -= buildings.grandma.price;
-                state.buildings.grandma.price = buildings.grandma.price * 1.15;
+            } else {
+                /*
+                Incrémente les constructions si le compteur est suffisant pour acheter la construction
+                 */
+                if (cookiesCount >= buildings[selectedBuilding].price) {
+                    buildings[selectedBuilding].count++;
+                    state.cookiesPerSecond += buildings[selectedBuilding].cps;
+                    state.cookiesCount -= buildings[selectedBuilding].price;
+                    buildings[selectedBuilding].price = buildings[selectedBuilding].price * 1.15;
+
+                    /*
+                    Récupération des améliorations qui peuvent être proposées au joueur
+                     */
+                    state.displayedUpgrades = [];
+                    Object.keys(upgrades).forEach(function (building) {
+                        let displayableUpgrades = _.filter(upgrades[building], function (upgrade) { return (!upgrade.active && upgrade.need <= buildings[building].count); })
+                        state.displayedUpgrades = _.sortBy(_.concat(state.displayedUpgrades, displayableUpgrades), 'price');
+                    })
+                }
             }
+        },
+
+        improvement(state, upgradeId) {
+            const {cookiesPerSecond, cookiesOnClick, buildings, upgrades} = state;
+
+            /*
+            Recherche de l'id de l'amélioration achetée pour l'activer et la retirer d'achat
+             */
+            Object.keys(upgrades).forEach(function (building) {
+                let selectedUpgrade = _.find(upgrades[building], {id: upgradeId});
+                if (selectedUpgrade) {
+                    selectedUpgrade.active = true;
+                    _.remove(state.displayedUpgrades, function (displayedUpgrade) {
+                        return displayedUpgrade.id === upgradeId;
+                    });
+
+                    state.cookiesPerSecond = cookiesPerSecond + buildings[building].cps;
+                    buildings[building].cps = buildings[building].cps * 2;
+
+                    /*
+                    Si le curseur est amélioré, le click sur le cookie l'est aussi
+                     */
+                    if (building === "cursor") (
+                        state.cookiesOnClick = cookiesOnClick * 2
+                    )
+                }
+            })
         }
     },
     actions: {}
